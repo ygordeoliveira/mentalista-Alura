a076456545c628090adf522b5ab174642648438f
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-04-16 16:40:43 -0300

    File of project

---
+++ script.js
@@ -0,0 +1,14 @@
+let numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+    let elementoResultado = document.getElementById("resultado");
+    let chute = parseInt(document.getElementById("valor").value);
+
+    if (chute === numeroSecreto) {
+        elementoResultado.innerHTML = "Você acertou!";
+    } else if (chute > 10 || chute < 0) {
+        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10!";
+    } else {
+        elementoResultado.innerHTML = "Errou, tente novamente!";
+    }
+}
